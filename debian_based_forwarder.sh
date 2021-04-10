ace6ca79435565904e378e9f2728a23d95430411
Nathan Melwani <melwaninate@gmail.com>
2021-04-10 02:16:39 -0700

    updated forwarder to send data every 30 seconds

--- debian_based_forwarder.sh
+++ debian_based_forwarder.sh
@@ -3,13 +3,16 @@
 HOST="192.168.1.71"
 PORT="8000"
 
-hostname=$(hostnamectl | grep "Static hostname" | awk 'BEGIN{FS=OFS=" ";}{print $3;}')
-uptime=$(uptime | awk 'BEGIN{FS=OFS=",";}{print $1;}' | xargs)
-ips=$(curl -s https://ipinfo.io/ip)
-osvar=$(lsb_release -d | awk 'BEGIN{FS=OFS=":";}{print $2;}' | xargs)
-release=$(lsb_release --r | awk 'BEGIN{FS=OFS=":";}{print $2;}' | xargs)
-packages=$(dpkg --list | wc --lines)
+while true
+do
+    hostname=$(hostnamectl | grep "Static hostname" | awk 'BEGIN{FS=OFS=" ";}{print $3;}')
+    uptime=$(uptime | awk 'BEGIN{FS=OFS=",";}{print $1;}' | xargs)
+    ips=$(curl -s https://ipinfo.io/ip)
+    osvar=$(lsb_release -d | awk 'BEGIN{FS=OFS=":";}{print $2;}' | xargs)
+    release=$(lsb_release --r | awk 'BEGIN{FS=OFS=":";}{print $2;}'# | xargs)
+    packages=$(dpkg --list | wc --lines)
 
-echo $os
+    curl --header "Content-Type: application/json" -d "{\"hostname\":\"$(hostname)\", \"os\":\"$osvar\", \"uptime\":\"$uptime\", \"release\":\"$release\", \"packages\":$packages}" -X POST http://$HOST:$PORT/endpoint
 
-curl --header "Content-Type: application/json" -d "{\"hostname\":\"$(hostname)\", \"os\":\"$osvar\", \"uptime\":\"$uptime\", \"release\":\"$(release)\", \"packages\":$packages}" -X POST http://$HOST:$PORT/endpoint
+    sleep 30
+done
