0441f5a17062def8abb88f6744b1181957d56acf
Anthony Hallak <ahall012@ucr.edu>
2021-04-11 03:03:55 -0700

    ajeet is 2cool

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,12 +14,41 @@ socket.addEventListener('close', () => {
   console.log('Websocket connection closed, refreshing page.')
 });
 
+// function useForceUpdate(){
+//   const [value, setValue] = React.useState(0); // integer state
+//   return () => setValue(value => value + 1); // update the state to force render
+// }
+
 export default function App() { //networks) {
-  const [ms, setMachines] = React.useState(new Map<string,machine>());
+
+  const [ms, setMachines] = React.useState([{
+    Uptime: "Uptime",
+    Hostname: "Hostname",
+    IP: "IP",
+    OS: "OS",
+    On: "1",
+    Ident: "id"
+  } as machine]);
+  
+  React.useEffect(() => {
+    setMachines(() => Object.values({}));
+  }, []);
 
   const updateMachine = (mInfo: machine) => {
-    ms.set(mInfo.Ident,mInfo);
-    setMachines(ms);
+    setMachines((prev: any) => { // supposed to be `: machine[]`
+      if (prev === undefined) prev = [];
+
+        for (let i = 0; i < prev.length; i++) {
+          if (prev[i].Ident === mInfo.Ident) {
+             prev[i] = mInfo;
+             return prev;
+          }
+        }
+        prev.push(mInfo);
+        console.log("Adding to array")
+        console.log(prev)
+        return prev;
+    });
   }
 
   socket.addEventListener('message', (e) => {
@@ -35,7 +64,7 @@ export default function App() { //networks) {
         break;
 
       case 'CURR_MACHINES':
-        setMachines(ms);
+        setMachines(() => Object.values(data.Data));
         break;
 
       case 'UPDATE_MACHINE':
@@ -55,7 +84,7 @@ export default function App() { //networks) {
     <div className="App">
       <header className="App-header">
         <h1>LAN-1</h1>
-          <MGrid inv={ms} key="MGrid"/>
+          <MGrid inv={ms}/>
       </header>
     </div>
   );
--- frontend/src/components/MGrid/MGrid.tsx
+++ frontend/src/components/MGrid/MGrid.tsx
@@ -17,27 +17,27 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 interface Props {
-  inv: Map<string, machine>,
-  key?: string
+  [inv: string]: machine[];
 }
 
 export default function MGrid({inv, key}: Props) {
   const classes = useStyles();
 
-  if (inv.size === 0) return <h2>No machines added yet!</h2>;
+  if (inv.length === 0) return <h2>No machines added yet!</h2>;
 
-  inv.forEach((v, k, m) => {
-    toAdd +=
-      <Grid key={v.Ident} item>
-        <Machine key={v.Ident+"machineee"} m={v}/>
-      </Grid>;
-  });
+  console.log(inv)
 
   return (
-      <Grid container className={classes.root} spacing={2} key={key}>
+      <Grid container className={classes.root} spacing={2}>
           <Grid item xs={12}>
               <Grid container justify="center" spacing={3}>
-                  {toAdd}
+                  {inv.map(e => {
+                    return (
+                      <Grid key={e.Ident+"griddd"} item>
+                        <Machine key={e.Ident+"machineee"} m={e}/>
+                      </Grid>
+                    );
+                  })}
               </Grid>
           </Grid>
       </Grid>  
